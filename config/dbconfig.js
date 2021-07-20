8c038a2522f9c6816f3f3c6781c8dc1ef9025f6d
yexiuph <yexiuph@gmail.com>
2021-07-20 12:48:00 +0800

    MSSQL Connection Fix

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -2,59 +2,94 @@ const YXPH = require('./YeXiuPH')
 const env = require('dotenv')
 
 const DB1Info = {
-    user: process.env.USER,
-    password: process.env.PASS,
-    server: process.env.SERVER,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
     database: process.env.DB1,
+    server: 'localhost',
     port: 1433,
-    encrypt: process.env.SQL_ENCRYPT,
-    trustServerCertificate: false // change to true for local dev / self-signed certs
-};
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+    options: {
+        encrypt: false, // for azure
+        trustServerCertificate: false // change to true for local dev / self-signed certs
+    }
+}
 
 const DB2Info = {
-    user: process.env.USER,
-    password: process.env.PASS,
-    server: process.env.SERVER,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
     database: process.env.DB2,
+    server: 'localhost',
     port: 1433,
-    encrypt: process.env.SQL_ENCRYPT,
-    trustServerCertificate: false // change to true for local dev / self-signed certs
-};
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+    options: {
+        encrypt: false, // for azure
+        trustServerCertificate: false // change to true for local dev / self-signed certs
+    }
+}
 
 const DB3Info = {
-    user: process.env.USER,
-    password: process.env.PASS,
-    server: process.env.SERVER,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
     database: process.env.DB3,
+    server: 'localhost',
     port: 1433,
-    encrypt: process.env.SQL_ENCRYPT,
-    trustServerCertificate: false // change to true for local dev / self-signed certs
-};
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+    options: {
+        encrypt: false, // for azure
+        trustServerCertificate: false // change to true for local dev / self-signed certs
+    }
+}
 
 const DB4Info = {
-    user: process.env.USER,
-    password: process.env.PASS,
-    server: process.env.SERVER,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
     database: process.env.DB4,
+    server: 'localhost',
     port: 1433,
-    encrypt: process.env.SQL_ENCRYPT,
-    trustServerCertificate: false // change to true for local dev / self-signed certs
-};
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+    options: {
+        encrypt: false, // for azure
+        trustServerCertificate: false // change to true for local dev / self-signed certs
+    }
+}
 
 const DB5Info = {
-    user: process.env.USER,
-    password: process.env.PASS,
-    server: process.env.SERVER,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
     database: process.env.DB5,
+    server: 'localhost',
     port: 1433,
-    encrypt: process.env.SQL_ENCRYPT,
-    trustServerCertificate: false // change to true for local dev / self-signed certs
-};
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+    options: {
+        encrypt: false, // for azure
+        trustServerCertificate: false // change to true for local dev / self-signed certs
+    }
+}
 
 module.exports = DBData = {
     DB1Info,
     DB2Info,
     DB3Info,
     DB4Info,
-    DB5Info,
+    DB5Info
 }
--- routes/Main.js
+++ routes/Main.js
@@ -1,22 +1,28 @@
 'use strict';
 const YXPH = require('../config/YeXiuPH')
 const main = YXPH.router
+const sql = require('mssql');
 const DB = YXPH.sql
 const DBConn = YXPH.dbconn
 
 // Get Server Info From DB5 ( API Database )
 main.get('/main', (req, res) => {
-    DB.connect(DBConn.DB5Info, (err) => {
+    DB.connect(DBConn.DB1Info, (err) => {
         if (err) {
-            res.send(`Unable to connect to Database "${DBConn.DB5Info.database}"`)
+            res.send(`Unable to connect to Database "${DBConn.Data1.database}"`)
             console.log(err)
+            DB.close()
         } else {
             res.send('DB Connected');
             //code for sql request here.
         }
 
     })
+})
+main.get('/testapi', (req, res) => {
 
 })
+
+
 
 module.exports = main
