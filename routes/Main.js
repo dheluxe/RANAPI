c84fc79f18d2b1b9d08ea9647bebc9070e03a7ab
yexiuph <yexiuph@gmail.com>
2021-07-20 10:04:30 +0800

    CI Fix

--- routes/Main.js
+++ routes/Main.js
@@ -1,11 +1,15 @@
 'use strict';
 const YXPH = require('../config/YeXiuPH')
 const main = YXPH.router
+const DB = YXPH.sql
+const DBConn = YXPH.dbconn
 
 
 
 main.get('/main', (req, res) => {
     console.log('Main Module Requested')
 })
+
+
 
 module.exports = main
