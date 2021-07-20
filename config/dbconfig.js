51c827b7b0c729546610bf78a425a15111801dd6
yexiuph <yexiuph@gmail.com>
2021-07-20 11:58:01 +0800

    Re-Test

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -2,48 +2,53 @@ const YXPH = require('./YeXiuPH')
 const env = require('dotenv')
 
 const DB1Info = {
-    "user": process.env.USER,
-    "password": process.env.PASS,
-    "server": process.env.SERVER,
-    "database": process.env.DB1,
-    "port": process.env.SERVERPORT,
-    "encrypt": process.env.SQL_ENCRYPT,
+    user: process.env.USER,
+    password: process.env.PASS,
+    server: process.env.SERVER,
+    database: process.env.DB1,
+    port: 1433,
+    encrypt: process.env.SQL_ENCRYPT,
+    trustServerCertificate: false // change to true for local dev / self-signed certs
 };
 
 const DB2Info = {
-    "user": process.env.USER,
-    "password": process.env.PASS,
-    "server": process.env.SERVER,
-    "database": process.env.DB2,
-    "port": process.env.SERVERPORT,
-    "encrypt": process.env.SQL_ENCRYPT,
+    user: process.env.USER,
+    password: process.env.PASS,
+    server: process.env.SERVER,
+    database: process.env.DB2,
+    port: 1433,
+    encrypt: process.env.SQL_ENCRYPT,
+    trustServerCertificate: false // change to true for local dev / self-signed certs
 };
 
 const DB3Info = {
-    "user": process.env.USER,
-    "password": process.env.PASS,
-    "server": process.env.SERVER,
-    "database": process.env.DB3,
-    "port": process.env.SERVERPORT,
-    "encrypt": process.env.SQL_ENCRYPT,
+    user: process.env.USER,
+    password: process.env.PASS,
+    server: process.env.SERVER,
+    database: process.env.DB3,
+    port: 1433,
+    encrypt: process.env.SQL_ENCRYPT,
+    trustServerCertificate: false // change to true for local dev / self-signed certs
 };
 
 const DB4Info = {
-    "user": process.env.USER,
-    "password": process.env.PASS,
-    "server": process.env.SERVER,
-    "database": process.env.DB4,
-    "port": process.env.SERVERPORT,
-    "encrypt": process.env.SQL_ENCRYPT,
+    user: process.env.USER,
+    password: process.env.PASS,
+    server: process.env.SERVER,
+    database: process.env.DB4,
+    port: 1433,
+    encrypt: process.env.SQL_ENCRYPT,
+    trustServerCertificate: false // change to true for local dev / self-signed certs
 };
 
 const DB5Info = {
-    "user": process.env.USER,
-    "password": process.env.PASS,
-    "server": process.env.SERVER,
-    "database": process.env.DB5,
-    "port": process.env.SERVERPORT,
+    user: process.env.USER,
+    password: process.env.PASS,
+    server: process.env.SERVER,
+    database: process.env.DB5,
+    port: 1433,
     encrypt: process.env.SQL_ENCRYPT,
+    trustServerCertificate: false // change to true for local dev / self-signed certs
 };
 
 module.exports = DBData = {
